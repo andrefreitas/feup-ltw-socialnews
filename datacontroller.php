6a9df2089b030e6162b3576e9609801912295600
André Freitas <p.andrefreitas@gmail.com>
2012-11-15 19:02:12 +0000

    Added functions in dataController

--- datacontroller.php
+++ datacontroller.php
@@ -1,4 +1,34 @@
 <?php
-echo "Hello world Arkadiusz";
 
+define('DBPATH','data/');
+
+class DataController{
+	
+	protected $dataBase;
+	
+	public function __construct($dataBase){
+		$dbPath='sqlite:'.DBPATH.$dataBase;
+		$this->dataBase = new PDO($dbPath);
+	}
+	
+	public function getNews(){
+		return $this->dataBase->query('SELECT * FROM news');
+	}
+	
+	public function getComments($newsId){
+		return $this->dataBase->query('SELECT * FROM comment where newsid='.$newsId);
+	}
+	
+	public function getNewsAuthor($newsId){
+		$author=$this->dataBase->query('SELECT userid FROM news where id='.$newsId);
+	}
+};
+
+$obj=new DataController('server.db');
+$result=$obj->getNews();
+$result=$obj->getComments(2);
+foreach( $result as $row) {
+	echo '<b>' . $row['content'] . ' </b><br/>';
+	}
+	
 ?>
