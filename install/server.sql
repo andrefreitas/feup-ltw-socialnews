9853e2bc9b96fc7c0a62de8aa11fdcf402c66b75
André Freitas <p.andrefreitas@gmail.com>
2012-11-15 00:16:08 +0000

    Added views and triggers in the database. Improved with metrics

--- install/server.sql
+++ install/server.sql
@@ -24,7 +24,9 @@ create table privilege(
 drop table if exists userActionPrivilege;
 create table userActionPrivilege (
 	privilegeId integer,
-	userActionId integer
+	userActionId integer,
+	foreign key(privilegeId) references privilege(id),
+	foreign key(userActionId) references userAction(id)
 );
 
 /* User */
@@ -34,7 +36,9 @@ create table user(
 	name text,
 	email text,
 	password text,
-	privilegeId integer
+	privilegeId integer,
+	picture text,
+	foreign key(privilegeId) references privilege(id)
 );
 
 /* News*/
@@ -46,7 +50,10 @@ create table news(
 	datePosted date,
 	timePosted text,
 	content text,
-	userId integer
+	userId integer,
+	metricsId integer,
+	foreign key(userId) references user(id),
+	foreign key(metricsId) references newsMetrics(id)
 );
 
 /* Comment */
@@ -57,7 +64,9 @@ create table comment(
 	datePosted date,
 	timePosted text,
 	userId integer,
-	newId integer
+	newsId integer,
+	foreign key(userId) references user(id),
+	foreign key(newsId) references news(id) 
 );
 
 /* Favorites */
@@ -78,8 +87,11 @@ create table tag(
 /* Tags * - * News */
 drop table if exists newsTag;
 create table newsTag(
-	newId integer,
-	tagId integer
+	newsId integer,
+	tagId integer,
+	primary key (newsId,tagId),
+	foreign key(newsId) references news(id),
+	foreign key(tagId) references tag(id)
 );
 
 /* List of external server with news web services*/
@@ -89,3 +101,74 @@ create table newsServer(
 	name text,
 	apiurl text
 );
+
+
+/* Metrics*/
+drop table if exists newsMetrics;
+create table newsMetrics(
+	id integer primary key autoincrement,
+	newsId integer,
+	pageviews integer,
+	searchs integer,
+	comments integer,
+	foreign key(newsId) references news(id)
+);
+
+/* List latest news */
+drop view if exists latestNews;
+create view latestNews As 
+	select * from news order by datePosted;
+
+/* List top commenters */
+drop view if exists topCommenters;
+create view topCommenters As
+	select user.name,user.id, count(*) as total
+	from user,comment
+	where user.id=comment.userId
+	group by user.id
+	order by count(*) DESC;
+
+/* List the top tags */
+drop view if exists topTags;
+create view topTags As
+	select tag.name,tag.id, count(*) as totalUsed
+	from tag,newsTag
+	where tag.id=newsTag.tagId
+	group by tag.id
+	order by count(*) DESC;
+
+/* Trigger for metrics */
+create trigger updateNewsMetrics 
+after insert on comment 
+  BEGIN
+    update newsMetrics
+    set comments=comments+1
+    where newsId=new.newsId;
+  END;
+
+create trigger createNewsMetrics 
+after insert on news
+  BEGIN
+    insert into newsMetrics(newsId,pageviews,searchs,comments)
+    values (new.id,0,0,0);
+  END;
+
+/* Tests 
+
+insert into user(name) values('Joao');
+insert into user(name) values('Carlos');
+insert into user(name) values('Maria');
+
+insert into comment(userId) values(2);
+insert into comment(userId) values(3);
+insert into comment(userId) values(3);
+
+insert into tag(name) values("sport");
+insert into tag(name) values("cars");
+
+insert into newsTag(tagId) values(1);
+insert into newsTag(tagId) values(2);
+insert into newsTag(tagId) values(2);
+
+insert into newsTag(tagId) values(2);
+	*/
